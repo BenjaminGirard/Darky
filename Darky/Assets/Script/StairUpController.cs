4b001482267deeb7f3b19a3f2f328e7f20dbd3bf
Benjamin GIRARD <bgirard@lithium.paris>
2018-11-04 22:24:20 +0900

    hot fix player not falling anymore

--- Darky/Assets/Script/StairUpController.cs
+++ Darky/Assets/Script/StairUpController.cs
@@ -6,14 +6,18 @@ namespace Script
 		public void OnTriggerStay2D(Collider2D other)
 		{
 			var rigBody = other.GetComponent<Rigidbody2D>();
+			var animator = other.GetComponent<Animator>();
 
+			animator.applyRootMotion = true;
 			rigBody.gravityScale = 0;
 		}
 
 		private void OnTriggerExit2D(Collider2D other)
 		{
 			var rigBody = other.GetComponent<Rigidbody2D>();
+			var animator = other.GetComponent<Animator>();
 
+			animator.applyRootMotion = false;
 			rigBody.gravityScale = 1;
 		}
 	}
