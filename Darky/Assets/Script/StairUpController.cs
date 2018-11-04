d0033437e518783629b06928157df89806f2ae0f
Benjamin GIRARD <bgirard@lithium.paris>
2018-11-04 22:12:08 +0900

    gravity fix

---
+++ Darky/Assets/Script/StairUpController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+	public class StairUpController : MonoBehaviour {
+		public void OnTriggerStay2D(Collider2D other)
+		{
+			var rigBody = other.GetComponent<Rigidbody2D>();
+
+			rigBody.gravityScale = 0;
+		}
+
+		private void OnTriggerExit2D(Collider2D other)
+		{
+			var rigBody = other.GetComponent<Rigidbody2D>();
+
+			rigBody.gravityScale = 1;
+		}
+	}
+}
