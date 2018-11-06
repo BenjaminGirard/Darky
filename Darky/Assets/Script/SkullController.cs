3312c8b078c1f53a766581bc88854c93bd1e532b
pereir_u <ugo.pereira@epitech.eu>
2018-11-06 07:24:14 +0100

    hitbox

---
+++ Darky/Assets/Script/SkullController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullController : MonoBehaviour {
+
+    Vector3 pos;
+    public float speed = 10f;
+    public float frequency = 10f;
+    public float magnitude = 10f;
+
+    void Start () {
+        pos = transform.position;
+	}
+	
+	void Update () {
+        pos -= transform.right * Time.deltaTime * speed;
+        transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+
+	}
+}
