eaf947dc8adca2d68da3b11d0d699d7648c8a88b
Benjamin GIRARD <bgirard@lithium.paris>
2018-11-06 15:22:50 +0900

    added demon movement

---
+++ Darky/Assets/Script/DemonMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonMove : MonoBehaviour
+{
+
+	public GameObject player;
+	private Animator anim;
+	
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis("Horizontal");
+
+		movement(hori);
+		anim.SetFloat("Hori", hori);
+		//anim.SetBool("Att", Att);
+	}
+
+	void movement(float hori)
+	{
+
+		if (transform.position.x < player.transform.position.x)
+		{
+			transform.Translate(Vector2.right * 2f * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 0);
+		}
+		else 
+		{
+			transform.Translate(Vector2.left * 2f * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 0);
+		}
+	}
+
+
+
+}
